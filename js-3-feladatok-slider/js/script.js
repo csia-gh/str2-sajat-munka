b1b8e5d3a07f228fae82aec37590b63b637903b1
Cserhati Imre Andras <cserhati.imre.andras@gmail.com>
2022-02-15 12:29:55 +0100

    imgs urls fixed in js-3-feladatok slider

--- js-3-feladatok-slider/js/script.js
+++ js-3-feladatok-slider/js/script.js
@@ -7,8 +7,8 @@ new Slider({
     './imgs/photo2.jfif',
     './imgs/photo3.jfif',
     './imgs/photo4.jfif',
-    './imgs/photo4.jfif',
-    './imgs/photo4.jfif',
+    './imgs/photo5.jfif',
+    './imgs/photo6.jfif',
   ],
   interval: 2500,
 });
